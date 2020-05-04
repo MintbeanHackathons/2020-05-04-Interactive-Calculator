388ef10ad072b72a10742b7cec787ca768b020b9
Joshua Fermin <Joshuaf91@gmail.com>
2020-05-04 14:58:20 -0400

    3pm cut off time

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const App = () => {
+    return (
+        <div>Hello world</div>
+    );
+}
+
+
+const domContainer = document.querySelector('app');
+ReactDOM.render(e(App), domContainer);
